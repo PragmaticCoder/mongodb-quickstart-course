40c0cb2e8eff965605c2599faad36565d6955aa9
Alvi Akbar <alvi.akbar@realpythonengineer.com>
2020-05-09 22:18:10 -0600

    added configuration for remote mongo host setup

--- src/starter_code_snake_bnb/src/data/mongo_setup.py
+++ src/starter_code_snake_bnb/src/data/mongo_setup.py
@@ -1,5 +1,21 @@
+import os
+import ssl
+
 import mongoengine
+
+alias_core = 'core'
+db_name = 'snake_bnb'
 
 
 def global_init():
-    mongoengine.register_connection(alias='core', name='snake_bnb')
+    data = dict(
+        username=os.environ.get('MONGODB_USERNAME', 'root'),
+        password=os.environ.get('MONGODB_PASSWORD', ''),
+        host=os.environ.get('MONGODB_HOST', '127.0.0.1'),
+        port=os.environ.get('MONGODB_PORT', 27017),
+        authentication_source='admin',
+        authentication_mechanism='SCRAM-SHA-1',
+        ssl=True,
+        ssl_cert_reqs=ssl.CERT_NONE
+    )
+    mongoengine.register_connection(alias=alias_core, name=db_name, **data)
