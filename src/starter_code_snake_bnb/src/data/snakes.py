4646ebd37ee1fefc7259fd5888c851690985afb7
Alvi Akbar <alvi@solusguard.com>
2020-05-09 21:19:15 -0600

    added skeleton class for snake model

---
+++ src/starter_code_snake_bnb/src/data/snakes.py
@@ -0,0 +1,12 @@
+class Snake:
+    registered_date = None
+    species = None
+
+    length = None
+    name = None
+    is_venomous = None
+
+    meta_data = {
+        'db_alias': '',
+        'collection': 'snake',
+    }
