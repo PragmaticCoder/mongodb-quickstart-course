4cbfacba37abc93876c39f340706728c47e50b7a
Alvi Akbar <alvi.akbar@realpythonengineer.com>
2020-05-09 23:05:15 -0600

    minor code refactoring

--- src/starter_code_snake_bnb/src/data/mongo_setup.py
+++ src/starter_code_snake_bnb/src/data/mongo_setup.py
@@ -11,10 +11,13 @@ def global_init():
     data = dict(
         username=os.environ.get('MONGODB_USERNAME', 'root'),
         password=os.environ.get('MONGODB_PASSWORD', ''),
+
         host=os.environ.get('MONGODB_HOST', '127.0.0.1'),
         port=os.environ.get('MONGODB_PORT', 27017),
+
         authentication_source='admin',
         authentication_mechanism='SCRAM-SHA-1',
+
         ssl=True,
         ssl_cert_reqs=ssl.CERT_NONE
     )
