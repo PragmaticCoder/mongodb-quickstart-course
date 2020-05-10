d33d25598e837ee1430c856db58578b2d2f51888
Alvi Akbar <alvi.akbar@realpythonengineer.com>
2020-05-09 23:05:59 -0600

    added field types using mongoengine

--- src/starter_code_snake_bnb/src/data/snakes.py
+++ src/starter_code_snake_bnb/src/data/snakes.py
@@ -1,10 +1,15 @@
-class Snake:
-    species = None
-    registered_date = None
+from datetime import datetime
 
-    length = None
-    name = None
-    is_venomous = None
+import mongoengine
+
+
+class Snake:
+    name = mongoengine.StringField(required=True)
+    species = mongoengine.StringField(required=True)
+
+    length = mongoengine.FloatField(required=True, min_value=0.00)
+    is_venomous = mongoengine.BooleanField(required=True)
+    registered_date = mongoengine.DateTimeField(default=datetime.now())
 
     meta_data = {
         'db_alias': 'core',
